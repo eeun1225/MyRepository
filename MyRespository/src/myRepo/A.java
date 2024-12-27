8af76796c017317aa068447d36bdd78aa5de6704
eeun1225 <eeun9108@gmail.com>
2024-12-28 00:55:40 +0900

    initial setting

---
+++ MyRespository/src/myRepo/A.java
@@ -0,0 +1,7 @@
+package myRepo;
+
+public class A {
+	public static void main(String[] args) {
+		System.out.println("Hello A");
+	}
+}
---
+++ MyRespository/src/myRepo/B.java
@@ -0,0 +1,8 @@
+package myRepo;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("Hello B");
+	}
+
+}
